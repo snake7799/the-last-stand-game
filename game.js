6c22dcff3653777801e413c1f61ebb62e6f90cb1
Pavel Egorov <snake7799@users.noreply.github.com>
2017-03-28 13:29:20 +0300

    simple-game/fix: some minor improvements

--- game.js
+++ game.js
@@ -1,108 +1,124 @@
-class player {
-    constructor() {
-        this.plPosX = 1000;
-        this.plPosY = 380;
-        this.playerPics = ['./Imgs/PlayerPics/Player Frame1.png',
-            './Imgs/PlayerPics/Player Frame2.png',
-            './Imgs/PlayerPics/Player Frame3.png',
-            './Imgs/PlayerPics/Player Frame4.png',
-            './Imgs/PlayerPics/Player Frame5.png',
-            './Imgs/PlayerPics/Player Frame6.png',
-            './Imgs/PlayerPics/Player Frame7.png',
-            './Imgs/PlayerPics/Player Frame8.png',
-            './Imgs/PlayerPics/Player Frame9.png',
-            './Imgs/PlayerPics/Player Frame10.png'
+var reqAnimFrame = window.requestAnimationFrame ||
+	window.mozRequestAnimationFrame 			||
+	window.webkitRequestAnimationFrame 			||
+	window.oRequestAnimationFrame 				||
+	window.msRequestAnimationFrame 				||
+	function (callback) {
+		window.setTimeout(callback, 1000 / 60);
+	};
+window.requestAnimationFrame = reqAnimFrame;
+
+class Player {
+	constructor() {
+		this.posX = 1000;
+		this.posY = 380;
+		this.frames = ['./img/player/player_frame1.png',
+            './img/player/player_frame2.png',
+            './img/player/player_frame3.png',
+            './img/player/player_frame4.png',
+            './img/player/player_frame5.png',
+            './img/player/player_frame6.png',
+            './img/player/player_frame7.png',
+            './img/player/player_frame8.png',
+            './img/player/player_frame9.png',
+            './img/player/player_frame10.png'
         ];
-        this.curFrame = 0;
-        this.curFrameInterval = 0;
-        this.shootSpeed = 20;
-        this.shootInterval = 0;
-        this.damage = 1;
-    }
+		this.curFrame = 0;
+		this.curFrameInterval = 0;
+		this.speed = 1.5;	
+	}
 }
 
-class shoot {
+class Shoot {
     constructor() {
         this.shootX = [];
         this.shootY = [];
+		this.speed = 20;
+		this.interval = 0;
+		this.damage = 1;
         this.isShot = false;
     }
+	
+	delay() {
+		return (this.speed > this.interval) ? false : true;
+	}
 }
 
-class enemy {
+class Enemy {
     constructor(posY) {
 
     }
 }
 
+
 var field = document.getElementById('gameArea');
-var pl;
-var Shoot;
-var shotImg = new Image();
 var ctx = field.getContext('2d');
-var plImg = new Image();
-var backgoundPic = new Image();
+var player;
+var shoot;
+var playerImg = new Image();
+var shotImg = new Image();
+var background = new Image();
 
 var beginGame = function() {
-    pl = new player();
-    Shoot = new shoot();
-    shotImg.src = './Imgs/PlayerPics/Weapon 1 Fire.png';
-    backgoundPic.src = './Imgs/Background.jpg';
-    plImg.src = './Imgs/PlayerPics/Player 1 stop.png';
-}
+    player = new Player();
+    shoot = new Shoot();
+	playerImg.src = './img/player/player_stop.png';
+	shotImg.src = './img/player/weapon_fire.png';
+    background.src = './img/background.jpg';
+};
 
 var enemyGeneration = function() {
 
-}
+};
 
 var enemyRedraw = function(context) {
 
-}
+};
 
 var playerRedraw = function(context) {
-    context.drawImage(backgoundPic, 0, 0, 1280, 640);
-    context.drawImage(plImg, pl.plPosX, pl.plPosY);
-    if (Shoot.isShot == true) {
-        for (var i = 0; i < Shoot.shootX.length; i++) {
-            if (Shoot.shootX[i] > 0) {
-                context.drawImage(shotImg, Shoot.shootX[i], Shoot.shootY[i]);
-                Shoot.shootX[i] -= 10;
+    context.drawImage(background, 0, 0, 1280, 640);
+    context.drawImage(playerImg, player.posX, player.posY);
+    if (shoot.isShot == true) {
+        for (var i = 0; i < shoot.shootX.length; i++) {
+            if (shoot.shootX[i] > 0) {
+                context.drawImage(shotImg, shoot.shootX[i], shoot.shootY[i]);
+                shoot.shootX[i] -= 10;
             } else {
-                Shoot.shootX.shift();
-                Shoot.shootY.shift();
+                shoot.shootX.shift();
+                shoot.shootY.shift();
             }
         }
     }
-}
+};
 
 
-function chengePlayerFrame() {
-    if (pl.curFrame == 9 & pl.curFrameInterval == 3) {
-        pl.curFrame = 0;
-        pl.curFrameInterval = 0;
-        plImg.src = pl.playerPics[pl.curFrame];
+function playerFrameChange() {
+    if (player.curFrame == 9 & player.curFrameInterval == 3) {
+        player.curFrame = 0;
+        player.curFrameInterval = 0;
+        playerImg.src = player.frames[player.curFrame];
     }
-    if (pl.curFrameInterval == 3) {
-        pl.curFrame++;
-        pl.curFrameInterval = 0;
-        plImg.src = pl.playerPics[pl.curFrame];
+    if (player.curFrameInterval == 3) {
+        player.curFrame++;
+        player.curFrameInterval = 0;
+        playerImg.src = player.frames[player.curFrame];
     }
-    pl.curFrameInterval++;
-}
+    player.curFrameInterval++;
+};
 
-function chengePlayerFrameBackword() {
-    if (pl.curFrame == 0 & pl.curFrameInterval == 3) {
-        pl.curFrame = 9;
-        pl.curFrameInterval = 0;
-        plImg.src = pl.playerPics[pl.curFrame];
+function playerFrameChangeBackwards() {
+    if (player.curFrame == 0 & player.curFrameInterval == 3) {
+        player.curFrame = 9;
+        player.curFrameInterval = 0;
+        playerImg.src = player.frames[player.curFrame];
     }
-    if (pl.curFrameInterval == 3) {
-        pl.curFrame--;
-        pl.curFrameInterval = 0;
-        plImg.src = pl.playerPics[pl.curFrame];
+    if (player.curFrameInterval == 3) {
+        player.curFrame--;
+        player.curFrameInterval = 0;
+        playerImg.src = player.frames[player.curFrame];
     }
-    pl.curFrameInterval++;
-}
+    player.curFrameInterval++;
+};
 
 var keyState = {};
 
@@ -110,80 +126,64 @@ document.addEventListener('keydown', function(e) {
     keyState[e.keyCode || e.which] = true;
 }, true);
 
-window.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function(e) {
     keyState[e.keyCode || e.which] = false;
 }, true);
 
-var shootDelay = function(delay) {
-    if (delay > pl.shootInterval) {
-        return false;
-    } else {
-        return true;
-    }
-}
-
 document.onkeyup = function playerDefaltActions(e) {
-    if (e.keyCode == 38 & pl.plPosY > 281) {
-        plImg.src = './Imgs/PlayerPics/Player 1 stop.png';
-    }
-    if (e.keyCode == 40 & pl.plPosY < 530) {
-        plImg.src = './Imgs/PlayerPics/Player 1 stop.png';
-    }
-    if (e.keyCode == 37 & pl.plPosX > 0) {
-        plImg.src = './Imgs/PlayerPics/Player 1 stop.png';
-    }
-    if (e.keyCode == 39 & pl.plPosX < 1200) {
-        plImg.src = './Imgs/PlayerPics/Player 1 stop.png';
-    }
-    if (e.keyCode == 32) {
-        plImg.src = './Imgs/PlayerPics/Player 1 stop.png';
-    }
-}
+	if ((e.keyCode == 38 && player.posY > 281) 	||
+		(e.keyCode == 40 && player.posY < 530) 	||
+		(e.keyCode == 37 && player.posX > 0)	||
+		(e.keyCode == 39 && player.posX < 1200) ||
+		(e.keyCode == 32)) {
+		playerImg.src = './img/player/player_stop.png';
+	}
+};
 
-var playerActions = function(){
-  if (keyState[38] && keyState[37] && pl.plPosY > 281 && pl.plPosX > 0){
-    pl.plPosX -= 1.5;
-    pl.plPosY -= 1.5;
-    chengePlayerFrame();
-  } else if (keyState[40] && keyState[37] && pl.plPosY < 530 && pl.plPosX > 0){
-    pl.plPosX -= 1.5;
-    pl.plPosY += 1.5;
-    chengePlayerFrame();
-  } else if (keyState[40] && keyState[39] && pl.plPosY < 530 && pl.plPosX < 1200){
-    pl.plPosX += 1.5;
-    pl.plPosY += 1.5;
-    chengePlayerFrameBackword();
-  } else if (keyState[38] && keyState[39] && pl.plPosY > 281 && pl.plPosX < 1200){
-    pl.plPosX += 1.5;
-    pl.plPosY -= 1.5;
-    chengePlayerFrameBackword();
-  } else if (keyState[37] && pl.plPosX > 0) {
-      pl.plPosX -= 3;
-      chengePlayerFrame();
-  } else if (keyState[39] && pl.plPosX < 1200) {
-      pl.plPosX += 3;
-      chengePlayerFrameBackword();
-  } else if (keyState[40] && pl.plPosY < 530) {
-      pl.plPosY += 3;
-      chengePlayerFrame();
-  } else if (keyState[38] && pl.plPosY > 281) {
-      pl.plPosY -= 3;
-      chengePlayerFrame();
-  } else if (keyState[32]) {
-      if (shootDelay(pl.shootSpeed) != false) {
-          Shoot.isShot = true;
-          pl.shootInterval = 0;
-          plImg.src = './Imgs/PlayerPics/Player 1 shoot.png';
-          Shoot.shootX.push(pl.plPosX);
-          Shoot.shootY.push(pl.plPosY + 17);
-      }
-  }
-}
+var playerActions = function () {
+	if (keyState[38] && keyState[37] && player.posY > 281 && player.posX > 0) {
+		player.posX -= player.speed;
+		player.posY -= player.speed;
+		playerFrameChange();
+	} else if (keyState[40] && keyState[37] && player.posY < 530 && player.posX > 0) {
+		player.posX -= player.speed;
+		player.posY += player.speed;
+		playerFrameChange();
+	} else if (keyState[40] && keyState[39] && player.posY < 530 && player.posX < 1200) {
+		player.posX += player.speed;
+		player.posY += player.speed;
+		playerFrameChangeBackwards();
+	} else if (keyState[38] && keyState[39] && player.posY > 281 && player.posX < 1200) {
+		player.posX += player.speed;
+		player.posY -= player.speed;
+		playerFrameChangeBackwards();
+	} else if (keyState[37] && player.posX > 0) {
+		player.posX -= player.speed * 2;
+		playerFrameChange();
+	} else if (keyState[39] && player.posX < 1200) {
+		player.posX += player.speed * 2;
+		playerFrameChangeBackwards();
+	} else if (keyState[40] && player.posY < 530) {
+		player.posY += player.speed * 2;
+		playerFrameChange();
+	} else if (keyState[38] && player.posY > 281) {
+		player.posY -= player.speed * 2;
+		playerFrameChange();
+	} else if (keyState[32]) {
+		if (shoot.delay() != false) {
+			shoot.isShot = true;
+			shoot.interval = 0;
+			playerImg.src = './img/player/player_shoot.png';
+			shoot.shootX.push(player.posX);
+			shoot.shootY.push(player.posY + 17);
+		}
+	}
+};
 
 requestAnimationFrame(function redraw() {
-    pl.shootInterval++;
+    shoot.interval++;
     playerActions();
     playerRedraw(ctx);
     enemyRedraw(ctx);
-    requestAnimationFrame(redraw);
-})
+	requestAnimationFrame(redraw);
+});
