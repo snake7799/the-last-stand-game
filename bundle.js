6f5719ae4b5d6c81a737f1bf9c1aefee30a99a4c
LachesisPW <LachesisPW@users.noreply.github.com>
2017-04-04 20:11:28 +0300

    fix: player death animation

--- bundle.js
+++ bundle.js
@@ -194,11 +194,11 @@ class Player extends Creature {
             this.run(0, this.speed * 1.7);
         } else if (keyState[38] && this.y > 244 && this.canMoveUp == true && this.isDead == false) {
             this.run(0, -this.speed * 1.7);
-        } else if (this.currentFrames === this.frameSets.run && this.isDead == false) {
+        } else if (!keyState[32] && this.currentFrames === this.frameSets.run && this.isDead == false) {
             this.stand();
         } else if (this.currentFrames === this.frameSets.shoot && this.currentFrame < this.currentFrames.length - 1) {
             this.frameChange(true);
-        } else this.stand();
+        }
 
         if (keyState[32] && this.isDead == false) this.shoot();else if (keyState[49] && this.isDead == false) this.changeWeapon(0);else if (keyState[50] && this.isDead == false) this.changeWeapon(1);else if (keyState[51] && this.isDead == false) this.changeWeapon(2);
 
