eccd53e6dd87b640be9194b524c60f9539d1b81b
LachesisPW <LachesisPW@users.noreply.github.com>
2017-04-08 22:22:04 +0300

    fix: reloading bug

--- bundle.js
+++ bundle.js
@@ -490,9 +490,11 @@ class Gun extends ObjectGenerator {
     }
 
     reload() {
-        this.currentBulletsAmount = 0;
-        this.countdownStart = Date.now();
-        this.readyToShoot = false;
+        if (this.currentBulletsAmount !== 0) {
+            this.currentBulletsAmount = 0;
+            this.countdownStart = Date.now();
+            this.readyToShoot = false;
+        }
     }
 }
 
